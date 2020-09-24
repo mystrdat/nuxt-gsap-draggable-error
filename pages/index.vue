9173e9516755409fe88466b139ac0bcfd7867442
mystrdat <mystrdat@gmail.com>
2020-09-24 12:00:39 +0200

    text case

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    hello world
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+import Draggable from '~/static/Draggable.min'
+
+gsap.registerPlugin(Draggable)
+
+export default {}
+</script>
